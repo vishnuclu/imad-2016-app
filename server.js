0b6b1b92f930eef447ce2c8ec251e314fe45282e
vishnuclu <tejavishnu.p@gmail.com>
2016-11-01 23:31:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.use(morgan('combined'));
 
 app.get('/',function(req,res){
    res.sendFile(path.join(__dirname,'E-commerce','main.html'));
-   res.sendFile(path.join(__dirname,'E-commerce','main-style.css'));
 });
 
 app.get('/index.html',function(req,res){
