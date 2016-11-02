ddad3c25a9432a9fb8fc03c8998e72130b4d559b
vishnuclu <tejavishnu.p@gmail.com>
2016-11-03 03:05:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/',function(req,res){
    res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 //app.get('/main-style.css',function(req,res){
-   res.sendFile(path.join(__dirname,'E-commerce','main-style.css'));
-});
+//   res.sendFile(path.join(__dirname,'E-commerce','main-style.css'));
+//});
 app.get('/index.html',function(req,res){
     res.send('Article-one is requested and is served here');
 });
