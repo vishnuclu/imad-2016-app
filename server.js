22cbead970ed383338734068a1121b10da981444
vishnuclu <tejavishnu.p@gmail.com>
2016-11-03 09:36:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/',function(req,res){
    res.sendFile(path.join(__dirname,'E-commerce','main.html'));
 });
+app.get('/E-commerce/main.html',function(req,res){
+   res.sendFile(path.join(__dirname,'E-commerce','main.html'));
+});
 app.get('/E-commerce/main-style.css',function(req,res){
    res.sendFile(path.join(__dirname,'E-commerce','main-style.css'));
 });
